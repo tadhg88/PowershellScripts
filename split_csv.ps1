875b5235672a9d0eade02eedbc79eb36437a1f95
Tim Whelan <tim.whelan@citywonders.com>
2021-06-28 21:14:09 +0100

    renaming vars

--- split_csv.ps1
+++ split_csv.ps1
@@ -1,36 +1,37 @@
 Write-Host "Start script"
 
-Write-Host $PSScriptRoot
-
-#$CurrentLocation = Get-Location
-$FileLocation = -join ($PSScriptRoot, "\Log-20210622.csv")
+# set path to file to split
+$FileLocation = -join ($PSScriptRoot, "\LogfileToSplit.csv")
 Write-Host $FileLocation
 
 # Read parent CSV
 $InputFilename = Get-Content $FileLocation
 $OutputFilenamePattern = 'output_split_'
-$LineLimit = 40000
+$LineLimitPerFile = 40000
 
 # Initialize
-$line = 0
-$i = 0
-$file = 0
+$masterFileLineNum = 0
+$outputFileLine = 0
+$outputFileNum = 0
 $start = 0
 
 # Loop all text lines
-while ($line -le $InputFilename.Length) {
+while ($masterFileLineNum -le $InputFilename.Length) {
 
     # Generate child CSVs
-    if ($i -eq $LineLimit -Or $line -eq $InputFilename.Length) {
-        $file++
-        $Filename = -join ($PSScriptRoot, "\", "$OutputFilenamePattern$file.csv")
-        $InputFilename[$start..($line - 1)] | Out-File $Filename -Force
-        $start = $line;
-        $i = 0
+    if ($outputFileLine -eq $LineLimitPerFile -Or $masterFileLineNum -eq $InputFilename.Length) {
+        $outputFileNum++
+        $Filename = -join ($PSScriptRoot, "\", "$OutputFilenamePattern$outputFileNum.csv")
+        $InputFilename[$start..($masterFileLineNum - 1)] | Out-File $Filename -Force
+        
+        $start = $masterFileLineNum;
+        $outputFileLine = 0
         Write-Host "$Filename"
     }
 
     # Increment counters
-    $i++;
-    $line++
+    $outputFileLine++;
+    $masterFileLineNum++
 }
+
+Write-Host "Processing complete......"
