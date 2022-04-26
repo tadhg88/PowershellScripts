19c3424e7dd9c273f46295265501799eacf0155b
Tim Whelan <tim.whelan@citywonders.com>
2022-04-26 15:53:46 +0100

    update to ask user for path to remove files from

--- Delete Old Log Files/Remove_App_Log_Files.ps1
+++ Delete Old Log Files/Remove_App_Log_Files.ps1
@@ -1,25 +1,24 @@
-# Purpose : Deletes all files under C:\Logs and 
-#         : C:\logs folder than the specified number of days.
+# Purpose : Deletes files in received path 
+#         : that are older than the specified number of days.
 
-# Days to keep c:\logs files.
-$LogsRetention = 30
+# Number Days of log files to keep.
+$LogsRetention = 5
 
-# Make the retention times negative.
+$pathReceived = Read-Host "Please enter path"
+
+# Make days negative.
 $LogsRetention = $LogsRetention * -1
 
-# Delete the old Logs files.
-#$Logs = (Get-ChildItem C:\Logs -File -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays($LogsRetention) })
-#$Logs | Remove-Item
+# get list of files to delete
+$Logs = (Get-ChildItem $pathReceived -File -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays($LogsRetention) })
+Write-Output $Logs
 
-$Logs = (Get-ChildItem C:\Logs -File -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays($LogsRetention) })
-#Write-Output $Logs
+# get delete confirmation
+$deleteFiles = Read-Host "Would you like to delete the files y/n"
 
-Write-Output "test output"
-
-# foreach ($server in $Logs) {
-#     Write-Output ("--- Searching on Server: $server at: ")
-# }
-
-$Logs | Select-Object | Write-Output
-
-#$Logs | Remove-Item
+if ($deleteFiles = "y")
+{
+    # delete files
+    $Logs | Remove-Item
+    Write-Output ("*** Files deleted ***")
+}
